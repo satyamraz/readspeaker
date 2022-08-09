4ac9755412062cad281781c180beffd05d9b07a2
Arunit Mazumdar <arunit.mazumdar@learningmate.com>
2022-08-09 18:50:06 +0530

    Adding rs_preserve segments

--- src/ReadSpeakerReader.jsx
+++ src/ReadSpeakerReader.jsx
@@ -24,6 +24,7 @@ const ReadSpeakerReader = () => {
 
     return (
         <React.Fragment>
+            <h2>Without RS_PRESERVE CLASS</h2>
             <section className="readspeaker-button-section mt-3 mb-2" id="readspeakerContainer">
                 <Container>
 
@@ -41,6 +42,17 @@ const ReadSpeakerReader = () => {
                 </Container>
             </section>
 
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit. Tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse. Tristique risus nec feugiat in fermentum. Quisque id diam vel quam elementum pulvinar etiam non quam. Ut pharetra sit amet aliquam id. Accumsan tortor posuere ac ut consequat semper. Mauris augue neque gravida in fermentum et sollicitudin ac orci. Nunc eget lorem dolor sed viverra ipsum. Massa id neque aliquam vestibulum morbi blandit cursus. In pellentesque massa placerat duis. Sed adipiscing diam donec adipiscing tristique risus nec feugiat. Amet venenatis urna cursus eget nunc scelerisque viverra mauris. Ornare arcu odio ut sem nulla. Quis lectus nulla at volutpat diam ut.
+            </p>
+            <p>
+                Ultrices gravida dictum fusce ut placerat orci. Morbi quis commodo odio aenean sed. Tincidunt vitae semper quis lectus. Senectus et netus et malesuada. Elementum sagittis vitae et leo duis ut diam quam. A diam maecenas sed enim ut sem viverra. Enim praesent elementum facilisis leo vel. Bibendum est ultricies integer quis auctor elit sed. Vel orci porta non pulvinar neque laoreet suspendisse. Faucibus et molestie ac feugiat sed lectus vestibulum mattis. Quis viverra nibh cras pulvinar mattis nunc sed blandit libero. Tellus mauris a diam maecenas sed enim ut. Amet consectetur adipiscing elit duis tristique. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis. Quam vulputate dignissim suspendisse in est.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit. Tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse. Tristique risus nec feugiat in fermentum. Quisque id diam vel quam elementum pulvinar etiam non quam. Ut pharetra sit amet aliquam id. Accumsan tortor posuere ac ut consequat semper. Mauris augue neque gravida in fermentum et sollicitudin ac orci. Nunc eget lorem dolor sed viverra ipsum. Massa id neque aliquam vestibulum morbi blandit cursus. In pellentesque massa placerat duis. Sed adipiscing diam donec adipiscing tristique risus nec feugiat. Amet venenatis urna cursus eget nunc scelerisque viverra mauris. Ornare arcu odio ut sem nulla. Quis lectus nulla at volutpat diam ut.
+            </p>
+
+            <h2>With RS_PRESERVE CLASS</h2>
             <section className="readspeaker-button-section mt-3 mb-2" id="readspeakerContainerSecond">
 
                 <Container>
@@ -52,7 +64,9 @@ const ReadSpeakerReader = () => {
                         </a>
                     </div>
 
-                    <ReactQuill theme="snow" value={valueSecond} onChange={setValueSecond} />
+                    <div className="rs_preserve">
+                        <ReactQuill theme="snow" value={valueSecond} onChange={setValueSecond} />
+                    </div>
 
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
